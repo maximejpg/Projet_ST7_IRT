a25d197c7a56c518d41a76aed1b5600f2af8d9f2
mattcbt <72977915+mattcbt@users.noreply.github.com>
2022-02-24 14:11:43 +0100

    Add files via upload
    Working

--- facility_location_problem.py
+++ facility_location_problem.py
@@ -1,40 +1,32 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Feb 22 16:06:25 2022
-
-@author: Mattéo
-"""
-
 import numpy as np
 import cplex
 from collections import namedtuple
 
 
 #-----------------------------------------------------------------------------
-# Input variables
+# Input variables for example
 #-----------------------------------------------------------------------------
 
-Client = namedtuple('Client', ('num',           # Client number, i
-                               'demand',        # Demand of the client, d_{i}
-                               'gain'))         # Unitary gain to satistfy the demand, r_{i}
-                                    
+
+# List of facilities                                   
 Facility = namedtuple('Facility', ('num',         # Facility number, j
                                    'capacity',    # Supply of the facility, S_{j}
                                    'cost'))       # Facility opening cost, c_{j}
-
-# List of facilities
 Facilities = (Facility("1", 3, 480),
               Facility("2", 1, 200),
               Facility("3", 2, 320),
-              Facility("4", 4, 340),
-              Facility("5", 1, 300))
+              Facility("4", 4, 340),)
 n = len(Facilities)
 
 # List of clients
+Client = namedtuple('Client', ('num',           # Client number, i
+                               'demand',        # Demand of the client, d_{i}
+                               'gain'))         # Unitary gain to satistfy the demand, r_{i}
 Clients = (Client("1", 2, 200),
            Client("2", 5, 150),
            Client("3", 3, 175))
 m = len(Clients)
+D = sum(c.demand for c in Clients)
 
 # Unitary transport cost between Client i and Facility j, t_{i,j}
 t = [[20, 30, 50, 35, 60],
@@ -46,61 +38,85 @@ t = np.array(t)
 q = [[(Clients[i].gain-t[i,j])*Clients[i].demand for j in range(n)] for i in range(m)]
 q = np.array(q).astype("float")
 
+
 #-----------------------------------------------------------------------------
 # Build the model
 #-----------------------------------------------------------------------------
 
+def facility_location_problem(Facilities, Clients, q, eq = False) :
 
-## Create CPO model
-flp = cplex.Cplex()
-
-
-## Create the decisions variables
-
-# Whether or not facility j must be opened, x_{j}
-x = list(flp.variables.add(obj = [f.cost for f in Facilities],
-                           lb = [0]*n,
-                           ub = [1]*n,
-                           types = ["B"]*n))
-
-y = [[] for i in range(m)]
-
-for i in range(m):
-    y[i] = list(flp.variables.add(obj = -q[i],
-                                  lb = [0]*n,
-                                  ub = [1]*n))
-
-
-## Create the constraints
-
-# Facility j cannot supply more than S_{j} :
+    ## Create CPO model
+    flp = cplex.Cplex()
     
-for j in range(n):
-    ind = [y[i][j] for i in range(m)] + [1.0]
-    val = [c.demand for c in Clients] + [-Facilities[j].capacity]
-    flp.linear_constraints.add(lin_expr = [cplex.SparsePair(ind = ind, val = val)],
-                                senses = ["L"],
-                                rhs = [0.0])
     
-# The demands must be fullfiled
-for i in range(m):
-    flp.linear_constraints.add(lin_expr = [cplex.SparsePair(ind = y[i], val = [1.0]*n)],
-                                senses = ["L"],
-                                rhs = [1.0])
+    ## Create the decisions variables
     
-# No supply can come from a closed facility
-for i in range(m):
-    for j in range(n):
-        flp.linear_constraints.add(lin_expr = cplex.SparsePair(ind = [y[i,j], x[j]], val = [1.0, -1.0]),
-                                    senses = ["L"],
-                                    rhs = [0.0])
+    # Whether or not facility j must be opened, x_{j}
+    x = list(flp.variables.add(obj = [f.cost for f in Facilities],
+                               lb = [0]*n,
+                               ub = [1]*n,
+                               types = ["B"]*n))
+    
+    y = [[] for i in range(m)]
+    for i in range(m):
+        y[i] = list(flp.variables.add(obj = -q[i],
+                                      lb = [0]*n,
+                                      ub = [1]*n))
+    
+    ## Create the constraints
+    
+    # Facility j cannot supply more than S_{j} : 
+    flp.linear_constraints.add(lin_expr = [[[y[i][j] for i in range(m)],[c.demand for c in Clients]] for j in range(n)],
+                               senses = ["L"]*n,
+                               rhs = [f.capacity for f in Facilities]
+                               )
         
-# y_{i,j} are rates
-for i in range(m):
-    for j in range(n):
-        flp.linear_constraints.add(lin_expr = [cplex.SparsePair(ind = y[i,j], val = [1.0])],
-                                    senses = ["L"],
-                                    rhs = [1.0])
+    # Demands are fullfiled
+    if eq == False :
+        flp.linear_constraints.add(lin_expr = [[y[i],[1.0]*n] for i in range(m)],
+                                senses = ["L"]*m,                               
+                                rhs = [1.0]*m)
+    
+    else : 
+        flp.linear_constraints.add(lin_expr = [[y[i],[1.0]*n] for i in range(m)],
+                                    senses = ["E"]*m,                               
+                                    rhs = [1.0]*m)
+        
+    # No supply can come from a closed facility
+    for i in range(m):
+        for j in range(n):
+            flp.linear_constraints.add(lin_expr = [cplex.SparsePair(ind = [y[i][j], x[j]], val = [1.0, -1.0])],
+                                       senses = ["L"],
+                                       rhs = [0.0])
+    
+    ## Solve the model
+    
+    flp.solve()
+    return flp
 
-flp.solve()
 
+#-----------------------------------------------------------------------------
+# Get and print results
+#-----------------------------------------------------------------------------
+
+flp = facility_location_problem(Facilities, Clients, q, eq = True)
+    
+x_sol = flp.solution.get_values()[:n]
+y_sol = np.array([flp.solution.get_values()[n+(i*n):n+(i+1)*n] for i in range(m)])
+distr = np.array([y_sol[i]*Clients[i].demand for i in range(m)])
+
+# print(distr)
+
+for j in range(n):
+    if x_sol[j] == 0:
+        print("\nFacility " + str(j+1) + " is closed")
+    else :
+        print("\nFacility " + str(j+1) + " supplies : (" + str(100*round(sum(distr[:,j])/Facilities[j].capacity,2)) + "% stock used)")
+        for i in [i for i in range(m) if y_sol[i,j] > 0.0]:
+            print("   - Client " + str(i+1) + " at a rate of " + str(100*round(y_sol[i,j], 2)) + "%")
+print("\n"
+      + str(100*round(sum(sum(distr))/sum([Facilities[j].capacity for j in range(n)]), 2))
+      + "% capacity used\n"
+      + str(100*round(sum(sum(distr))/sum([Clients[i].demand for i in range(m)]), 2))
+      + "% delivery rate"
+      + "\nTotal profit: " + str(round(-flp.solution.get_objective_value(),2)))
